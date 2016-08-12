0617922619be4e00637fe3f14d81142ba9a558e2
Joshua Hagins <jhagins@palantir.com>
2016-08-12 02:50:24 -0400

    Add custom hub formula

---
+++ Formula/hub.rb
@@ -0,0 +1,29 @@
+class Hub < Formula
+  desc "Hub with PR templates"
+  homepage "https://hub.github.com/"
+  url "https://github.com/jez/hub.git", using: :git, revision: "0de949bf0684485bf353202bed2c42a659017594"
+  version "2.2.0-243-g0de949b"
+
+  option "without-completions", "Disable bash/zsh completions"
+
+  depends_on "go" => :build
+
+  def install
+    system "script/build", "-o", "hub"
+    bin.install "hub"
+    man1.install Dir["man/*"]
+
+    if build.with? "completions"
+      bash_completion.install "etc/hub.bash_completion.sh"
+      zsh_completion.install "etc/hub.zsh_completion" => "_hub"
+    end
+  end
+
+  test do
+    system "git", "init"
+    %w[haunted house].each { |f| touch testpath/f }
+    system "git", "add", "haunted", "house"
+    system "git", "commit", "-a", "-m", "Initial Commit"
+    assert_equal "haunted\nhouse", shell_output("#{bin}/hub ls-files").strip
+  end
+end
